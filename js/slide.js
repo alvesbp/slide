188e80ce746df11ef635f1e849110aa0f53bf6eb
Matheus Brito ££ <matheus.alvesdb@gmail.com>
2023-04-14 10:11:45 -0300

    feito calculo de distancia

--- js/slide.js
+++ js/slide.js
@@ -3,20 +3,31 @@ import debounce from "./debounce.js";
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
+
     this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
   }
 
   onStart(event) {
     event.preventDefault();
-    console.log("mousedown");
+    this.dist.startX = event.pageX;
+
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
 
   onMove(event) {
-    console.log("moveu");
+    this.dist.movement =
+      -(this.dist.startX - event.pageX) * 1.5 + this.dist.finalPosition;
+    this.slideMove();
+  }
+
+  slideMove() {
+    this.slide.style.transform = `translate3d(${this.dist.movement}px, 0, 0)`;
   }
 
   onEnd() {
+    this.dist.finalPosition = this.dist.movement;
+    this.dist.finalPosition = this.dist.finalPosition;
     this.wrapper.removeEventListener("mousemove", this.onMove);
   }
 
@@ -24,6 +35,7 @@ export default class Slide {
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
     this.onEnd = this.onEnd.bind(this);
+    this.slideMove = this.slideMove.bind(this);
   }
 
   addSlideEvents() {
